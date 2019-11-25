03ad3c6d49dfefb71f7f8a52cead2c4a37a60b6f
Daniel Luiz da Silva <daniel.luiz@matera.com>
2019-11-25 14:08:13 -0300

    alterando out

--- src/br/com/matera/Teste.java
+++ src/br/com/matera/Teste.java
@@ -1,8 +1,11 @@
 package br.com.matera;
 
+import java.time.LocalDate;
+
 public class Teste {
 
-	public static void main(String[] args) {
-		System.out.println("Hello word!!");
+    public static void main(String[] args) {
+
+		System.out.println(String.format("Hello word!! %s", LocalDate.now().toString()));
 	}
 }
